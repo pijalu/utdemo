afff190bafe187a082cbdd6237ed4cff49127a0e
Pierre Poissinger <Pierre.Poissinger@ordina.be>
2014-07-07 14:57:37 +0200

    Clarify the mocked random code

--- src/test/java/be/ordina/utdemo/factoids/service/RandomFactServiceTest.java
+++ src/test/java/be/ordina/utdemo/factoids/service/RandomFactServiceTest.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package be.ordina.utdemo.factoids.service;
 
@@ -18,14 +18,14 @@ import be.ordina.utdemo.factoids.provider.FactProvider;
 
 /**
  * Test random fact
- * 
+ *
  * @author ppoissinger
- * 
+ *
  */
 public class RandomFactServiceTest extends UnitilsJUnit4 {
     /**
      * Tested object
-     * 
+     *
      */
     private RandomFactService service;
 
@@ -44,9 +44,12 @@ public class RandomFactServiceTest extends UnitilsJUnit4 {
     @SneakyThrows
     public final void init() {
         // Setup mocks: Lock random
-        for (int i = 0; i < SIZE; ++i) {
-            random.returns(i).nextInt(i + 1);
-            factProvider.returns(new Fact(String.format("%d", i))).getFact(i);
+        // Return a predictable order: last to first
+        for (int factIndex = 0; factIndex < SIZE; ++factIndex) {
+            int leftToSelect = factIndex + 1;
+            random.returns(factIndex).nextInt(leftToSelect);
+            String factText = String.format("%d", factIndex);
+            factProvider.returns(new Fact(factText)).getFact(factIndex);
         }
         factProvider.returns(SIZE).size();
 
@@ -63,9 +66,10 @@ public class RandomFactServiceTest extends UnitilsJUnit4 {
      */
     @Test
     public final void testGetAFact() {
-        for (int i = SIZE - 1; i < 0; i--) {
-            Fact expected = new Fact(String.format("%d", i));
-            Assert.assertEquals(expected, service.getAFact());
+        for (int runIndex = SIZE - 1; runIndex < 0; runIndex--) {
+            String expectFactText = String.format("%d", runIndex);
+            Fact expectedFact = new Fact(expectFactText);
+            Assert.assertEquals(expectedFact, service.getAFact());
         }
     }
 
